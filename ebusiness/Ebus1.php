56554ab03bd98a86a0dd73153e6ad898027fad89
Sam Goggin <117486844@umail.ucc.ie>
2018-02-19 15:19:04 +0000

    pleasework

--- ebusiness/Ebus1.php
+++ ebusiness/Ebus1.php
@@ -14,7 +14,7 @@
         
         </br>
         
-        <form method="POST" action="Ebus2.php"
+        <form method="POST" action="Ebus2.php">
         
         <label for="salesforce">
             <input type="radio" id="salesforce" name="product" checked onClick="disablebtnProceed()"/>
